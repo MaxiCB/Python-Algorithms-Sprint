4bb6a36fce0b27e3baa3fc62d5ea4ec24c714fc8
Aaron Beasley <aaroncb@aaroncb.com>
2020-06-19 10:24:11 -0500

    ADD Count_TH
    - Added count_th functionality
    - Passing all test's

--- recursive_count/__init__.py
+++ recursive_count/__init__.py
@@ -6,6 +6,18 @@ Your function must utilize recursion. It cannot contain any loops.
 
 
 def count_th(word):
-    # TBC
-
-    pass
+    # Count Reference
+    count = 0
+    # If the length is less than 2 return count
+    if len(word) < 2:
+        return count
+    # Else check for 'th' using splicing
+    else:
+        # If found iterate count
+        if word[:2] == 'th':
+            count += 1
+    # Remove the first character from the string
+    word = word[1:]
+    # Recurse until the length is less that two
+    # Then return the total count
+    return count + count_th(word)
